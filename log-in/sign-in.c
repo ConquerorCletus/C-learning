a8e71311ede04bfff0f8b688ac1a1b7105ec841c
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-06 11:50:53 +0100

    log in

---
+++ log-in/sign-in.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+/**
+ * main - Get user signed in.
+ * Return:0
+ * Date:05-03-2023
+ */
+
+int main()
+{
+	char name[50];
+	char userName[20];
+	char passphrase[30];
+	char pass[30];
+	int a;
+
+
+	printf("input your full name:\n");
+        fgets(name, sizeof(name),stdin);
+
+	printf("input desired username:\n");
+        fgets(name, sizeof(name),stdin);
+
+	printf("Enter your passphrase(2):\n");
+	fgets(passphrase, sizeof(passphrase),stdin);
+	printf("Enter your passphrase again(1):\n");
+	fgets(pass, sizeof(pass),stdin);
+
+
+/*To make sure pass phrase are the same.*/
+	for (a = 0; passphrase[a] != '\0' && pass[a] != '\0'; a++)
+	{
+		/*if (name[a] == '\0')
+        {
+                printf("Empty field noticed!!!\n
+");
+		break;
+        }
+		else if (pass[a] =='\0')
+        {
+                printf("Empty field noticed!!!\n
+");
+		break;
+        }
+	else if (userName == '\0')
+        {
+                printf("Empty field noticed!!!\n");
+		break;
+        }
+	else if (passphrase[a] == '\0')
+        {
+                printf("Empty field noticed!!!\n");
+		break;
+        }*/
+
+	 if (passphrase[a] == pass[a])
+	{
+		printf("Sign-in successful!!\n");                              break;                                  } 
+	else if(passphrase[a] != pass[a])
+	{                                       
+	printf("Wrong passphrase!!!\n");
+	break;
+	}
+	}
+	return (0);
+}
